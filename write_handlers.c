b84f0598fccf00620a62b3d6d4838f6079ab99b7
bonphillip <136075793+bonphillip@users.noreply.github.com>
2023-08-22 15:44:57 +0300

    Update write_handlers.c

--- write_handlers.c
+++ write_handlers.c
@@ -9,7 +9,6 @@
  * @width: get width.
  * @precision: precision specifier
  * @size: Size specifier
- *
  * Return: Number of chars printed.
  */
 int handle_write_char(char c, char buffer[],
@@ -200,7 +199,7 @@ int write_unsgnd(int is_negative, int ind,
  * @buffer: Arrays of chars
  * @ind: Index at which the number starts in the buffer
  * @length: Length of number
- * @width: Wwidth specifier
+ * @width: Width specifier
  * @flags: Flags specifier
  * @padd: Char representing the padding
  * @extra_c: Char representing extra char
